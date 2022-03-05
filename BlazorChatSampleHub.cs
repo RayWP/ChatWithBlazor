ae20be4f0cd29c5eb1c091ffb9dfae68bec3ba22
raymond <180709879@students.uajy.ac.id>
2022-03-05 14:09:38 +0700

    first commit

---
+++ BlazorChatSampleHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat
+{
+    public class BlazorChatSampleHub: Hub
+    {
+        public const string HubUrl = "/chat";
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
